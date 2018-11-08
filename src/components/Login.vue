83800c5dc467cc9ffb4150d46f43d6c58d6ac0e8
mcqua007 <mcqua007@cougars.csusm.edu>
2018-11-07 17:14:58 -0800

    type: feat | finsihed setting up the sign up page and login page componenets

---
+++ src/components/Login.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="login">
+  <h3>Sign In</h3>
+  <input type="text" placeholder="email" />
+  <input type="password" placeholder="password" />
+  <button>connection</button>
+  <p>
+    No, Account? Create one!
+  </p>
+</div>
+</template>
+<script>
+export default {
+  name: 'login',
+  data: function() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+<style scoped>
+.login{
+  margin-top:40px;
+}
+input{
+  margin:10px 0;
+  width:20%;
+  padding:15px;
+}
+button{
+  margin-top:20px;
+  width:10%;
+  cursor: pointer;
+  padding:15px;
+}
+p{
+
+  margin-top:40px;
+  font-size:13px;
+}
+p a{
+  text-decoration:underline;
+  cursor:pointer;
+}
+
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="sign-up">
+  <h3>Welcome, Let's Sign Up</h3>
+  <input type="text" placeholder="email" />
+  <input type="password" placeholder="password" />
+  <button>Sign Up</button>
+  <p>
+    Go back to Login 
+  </p>
+</div>
+</template>
+<script>
+export default {
+  name: 'signUp',
+  data: function() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+<style scoped>
+.sign-up{
+  margin-top:40px;
+}
+input{
+  margin:10px 0;
+  width:20%;
+  padding:15px;
+}
+button{
+  margin-top:20px;
+  width:10%;
+  cursor: pointer;
+  padding:15px;
+}
+p{
+
+  margin-top:40px;
+  font-size:13px;
+}
+p a{
+  text-decoration:underline;
+  cursor:pointer;
+}
+
+</style>
