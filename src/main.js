bb49b9036bd6da1c3f3306a71425a249434a6cd4
Admin <admin@Qwertys-iMac.local>
2018-11-09 07:12:32 -0800

    type: feat | removed vulnrability

--- src/main.js
+++ src/main.js
@@ -9,14 +9,7 @@ Vue.config.productionTip = false
 
 // Initialize Firebase
 let app;
-let config = {
-  apiKey: "AIzaSyAsaYC79Qvy1OfcmREKnx5qDwyG7GdEo6U",
-  authDomain: "vue-auth-project-99810.firebaseapp.com",
-  databaseURL: "https://vue-auth-project-99810.firebaseio.com",
-  projectId: "vue-auth-project-99810",
-  storageBucket: "vue-auth-project-99810.appspot.com",
-  messagingSenderId: "255616009393"
-};
+
 firebase.initializeApp(config);
 
 firebase.auth().onAuthStateChanged(function(user) {
